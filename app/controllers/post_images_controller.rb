415a8b1ada65e3b5880e6178128c0f79906cd61a
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-18 11:54:30 +0000

    [Finish]post_images_controllerでnewアクションを定義

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,7 @@
 class PostImagesController < ApplicationController
   def new
+    # formに渡すための「空のモデル」を作成。@post_imageに代入。@のお陰でpost_imageをviewで使うことができる。
+    @post_image = PostImage.new
   end
 
   def index
