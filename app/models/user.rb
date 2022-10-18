810219e7c17e0f6bdd38def681d36b4021f5db1d
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-18 09:04:17 +0000

    [Update]UserモデルにPostImageモデルの紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  # 1対Nの関係。userは「1」
+  has_many :post_images, dependent: :destroy
 end
