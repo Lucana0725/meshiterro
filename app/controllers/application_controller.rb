5dc618c4c9e78b337104394ac5e75f3311f3fcad
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-18 07:26:56 +0000

    [Update]application_controller.rbにてログイン/アウト時の遷移ページをaboutへ変更の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  # ログイン後に遷移するページをtopからaboutへ変更(デフォルトはroot_path)
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  # ログアウト後に遷移するページをtopからaboutへ変更(デフォルトはroot_path)
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
+  
+  # 以下ストロングパラメータと同じような働き。deviseへの変更を許可する。
+  # privateではなくprotectedなのは、privateは同じコントローラー内からでしか参照できないが、
+  # protectedは別コントローラーでも参照できるから。
   protected
   
   def configure_permitted_parameters
