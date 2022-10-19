03e6fcc4c08c6f4fa32e30f088c546de76cc6d0b
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-19 01:26:59 +0000

    [Update]Userモデルのget_profile_imageにwidth, heightの引数を設定。リサイズを指定できるように改良

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,13 @@ class User < ApplicationRecord
   # profile_imageという名前で、ActiveStrageでプロフィール画像を保存できるように。
   has_one_attached :profile_image
   
-  プロフィール画像を100x100のサイズにリサイズして取得するメソッド
-  def get_profile_image
+  # プロフィール画像を100x100のサイズにリサイズして取得するメソッド
+  # 追記：メソッドに(width, height)の引数を持たせることで100x100だけでないサイズを実行時に指定できるよう改良
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).proccessed
+    profile_image.variant(resize_to_limit: [width, height]).proccessed
   end
 end
