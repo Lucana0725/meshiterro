3a13b8fb95fc2754f0a76e28b12773f53fa8312f
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-19 01:24:27 +0000

    [Finish]Userモデルにプロフィール画像を設定できるよう記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
          
   # 1対Nの関係。userは「1」
   has_many :post_images, dependent: :destroy
+  
+  # プロフィール画像をアップロードできるようにする
+  # profile_imageという名前で、ActiveStrageでプロフィール画像を保存できるように。
+  has_one_attached :profile_image
+  
+  プロフィール画像を100x100のサイズにリサイズして取得するメソッド
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).proccessed
+  end
 end
