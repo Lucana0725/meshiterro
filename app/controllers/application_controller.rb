e1371db9c880f4043507c8e4dff66b9fd11ce997
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-27 06:33:41 +0000

    [Update]ログインできなかったのを、application_controller内の新規登録認証キーを[:name] => [:email]に変更して解消。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,26 @@ class ApplicationController < ActionController::Base
   # ログイン認証が済んでいなければ(:authenticate_user!)、ログイン画面へリダイレクトする
   # exceptは指定したアクションをbefore_actionから外せる。今回はログイン前はtopページのみを表示したいのでexcept: [:top]
   before_action :authenticate_user!, except: [:top]
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   # ログイン後に遷移するページをtopからaboutへ変更(デフォルトはroot_path)
   def after_sign_in_path_for(resource)
     post_images_path
   end
-  
+
   # ログアウト後に遷移するページをtopからaboutへ変更(デフォルトはroot_path)
   def after_sign_out_path_for(resource)
     about_path
   end
-  
-  
+
+
   # 以下ストロングパラメータと同じような働き。deviseへの変更を許可する。
   # privateではなくprotectedなのは、privateは同じコントローラー内からでしか参照できないが、
   # protectedは別コントローラーでも参照できるから。
   protected
-  
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
